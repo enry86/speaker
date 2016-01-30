ff136161e3899e1bae21c9b08c209a07f0c5bb9f
Enrico Sartori <sartori.enrico@gmail.com>
2016-01-30 23:51:35 +0100

    pretty much everything

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>
+      Speaker
+    </title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/style.css"/>
+
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <script src="js/speaker.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header clearfix">
+	<nav>
+	  <ul class="nav nav-pills pull-right">
+	    <li role="presentation">
+	      <a href="#"
+		 data-toggle="modal"
+		 data-target="#info-modal">Come funziona?</a>
+	    </li>
+	  </ul>
+	  <h3><i class="fa fa-volume-up"></i>
+	    Speaker</h3>
+	  <hr>
+	</nav>
+      </div>
+      <div class="section" id="input-text">
+	<div class="row">
+	  <div class="col-sm-12 col-md-offset-3 col-md-6">
+	    <div class="panel panel-default">
+	      <div class="panel-heading">
+		<i class="fa fa-file-text-o"></i>
+		Inserisci qui il testo che non sai pronunciare:
+	      </div>
+	      <div class="panel-body">
+		<textarea id="ta-text"
+			  class="form-control"
+			  rows="15"></textarea>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<div class="row">
+	  <div class="col-md-12 text-center">
+	    <button class="btn btn-lg btn-success" id="btn-translate">
+	      <i class="fa fa-comment"></i>
+		Pronuncia!
+	    </button>
+	  </div>
+	</div>
+      </div>
+
+      <div class="section hide" id="output-resp">
+	<div class="row">
+	  <div class="col-sm-12 col-md-offset-3 col-md-6">
+	    <div class="panel panel-default">
+	      <div class="panel-heading">
+		<i class="fa fa-comment"></i>
+		Ecco come dovresti pronunciarlo
+	      </div>
+	      <div class="panel-body">
+		<div id="div-res"></div>
+	      </div>
+	    </div>
+	  </div> 	    	    
+	</div>
+	<div class="row">
+	  <div class="col-md-12 text-center">
+	    <button class="btn btn-lg btn-warning" id="btn-back">
+	      <i class="fa fa-arrow-left"></i>
+		Prova con un altro testo
+	    </button>
+	  </div>
+	</div>
+      </div>
+      
+      
+      <div class="footer">
+	<hr>
+	<p class="text-info">
+	  <i class="fa fa-smile-o"></i>
+	  Beh, se funziona meglio cos&igrave;... se no pazienza
+	</p>
+      </div>
+
+
+
+      <div class="modal fade"
+	   id="wait-modal"
+	   tabindex="-1"
+	   role="dialog"
+	   backdrop="static">
+	<div class="modal-dialog modal-sm">
+	  <div class="modal-content">
+	    <div class="modal-body text-center">
+              <h4>Un attimo di pazienza... <i class="fa fa-smile-o"></i>
+	      </h4>
+	      <div class="progress">
+		<div class="progress-bar" id="progress"
+		     role="progressbar"
+		     aria-valuenow="0"
+		     aria-valuemin="0"
+		     aria-valuemax="100"
+		     style="width: 0%;">
+		</div>
+	      </div>
+	    </div>	    
+	  </div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+
+
+      <div class="modal fade"
+	   id="def-modal"
+	   tabindex="-1"
+	   role="dialog">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+	    <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title" id="word-title"></h4>
+	    </div>	    	    
+	    <div class="modal-body">
+	      <div id="definitions">
+	      </div>	      
+	    </div>	    
+	  </div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+      
+      <div class="modal fade"
+	   id="info-modal"
+	   tabindex="-1"
+	   role="dialog">
+	<div class="modal-dialog">
+	  <div class="modal-content">
+	    <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">O meglio, come non funziona...</h4>
+	    </div>	    	    
+	    <div class="modal-body">
+	      <p>
+		Benvenuti in Speaker, che in realt&agrave; volevo chiamare
+		RoboticSilvia o qualcosa del genere, ma poi mi &egrave;
+		sembrato indelicato.
+	      </p>
+	      <p>
+		Questo semplice servizio ti permette
+		di ottenere da un dizionario la pronuncia corretta di
+		tutte le parole che compongono un testo.
+	      </p>
+	      <p>
+		Figo eh?
+	      </p>
+	      <p>
+		Per facilitarmi un po' la vita ho scelto di utilizzare
+		<a href="http://wordreference.com/definizione"
+		   target="_blank">
+		  WordReference.com
+		</a>
+		come dizionario di base.
+		Utilizzare il DOP sarebbe stato un mezzo dramma, per via
+		di una serie di succulenti dettagli tecnici che vi risparmio.
+	      </p>
+	      <p>
+		Ora, WordReference funziona abbastanza bene, per alcune
+		(poche) parole ha addirittura l'audio.
+		Purtroppo non riesce a dare la pronuncia corretta dei verbi
+		coniugati, d'altronde mi sembra che nemmeno il DOP lo faccia.
+		Quello che riesce a fare è risalire all'infinito di un
+		qualsiasi verbo, ossia se gli passo 'credette' lui mi porta
+		ai risultati del verbo 'credere', per&ograve; chiaramente
+		non mi da informazioni sulla pronuncia di 'credette'.
+	      </p>
+	      <p>
+		Altra cosa, se cercate una parola con più significati,
+		nel mostrare i risultati li espongo tutti, in modo che siate
+		voi a contestualizzare e capire qual &egrave; la parola che
+		stavate cercando. Insomma, mica posso fare tutto io.
+	      </p>
+	      <p>
+		Nella visualizzazione dei risultati, se premete su uno dei
+		significati di una parola, vi vengono fornite le definizioni
+		e, se disponibile, l'audio della pronuncia corretta.
+	      </p>
+	      <p>
+		Bene, pi&ugrave; o meno questo &egrave; quanto, have fun!
+	      </p>
+	    </div>	    
+	  </div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+      
+    </div>   
+  </body>
+</html>
+
+
---
+++ js/speaker.js
@@ -0,0 +1,274 @@
+//JS module for speaker app
+
+var BASE_URL_DICT = 'http://www.wordreference.com/definizione/';
+var BASE_URL = 'www.wordreference.com';
+var PRON_CLASS = '.Pron';
+var ENTRY_CLASS = '.entryitit';
+var SINGLE_DEF_CLASS = '.Definition';
+var MULTI_DEF_CLASS = '.definition';
+
+var separators = [',',' ',';','\\.','\\!','\\?','\\n','\\"','\\\''];
+var separators_str = ', ;.!?\n"\'';
+
+var currentTask = {
+    words : [],
+    translations : [],
+    processed_words : 0,
+    get_current_word: function() {
+	if (this.words.length > this.processed_words) {
+	    return this.words[this.processed_words];
+	}
+	else
+	    return null;
+    },
+    push_result: function(res) {
+	this.processed_words++;
+	this.translations.push(res);	
+    },
+    get_progress: function() {
+	if (this.processed_words == 0)
+	    return 0;
+	else {
+	    var res = this.processed_words / this.words.length;
+	    res = Math.floor(res);
+	    return res*100;
+	}
+    }
+};
+
+function translate() {
+    $('#wait-modal').modal();
+    var text = $('#ta-text').val();
+    var regexp = '(' + separators.join('|') + ')';
+    var re =  new RegExp(regexp);
+    var tokens = text.split(re);
+    resetTask();
+    tokens.forEach(updateTask);
+
+    runTaskStep();
+}
+
+function runTaskStep() {
+    var word = currentTask.get_current_word();
+    var url = BASE_URL_DICT + word;
+    if (word == null) {
+	completeTask();
+    }
+    else if (separators_str.indexOf(word) > 0) {
+	var res = getEmptyItem(word);
+	currentTask.push_result(res);
+	runTaskStep();
+    }
+    else {    
+	$.ajax(url, {
+	method : "GET",
+	    dataType : "html",
+	    success : succHandler,
+	    error : errHandler 
+	});
+    }
+}
+
+
+function completeTask() {
+    updateProgressbar();
+    $('#wait-modal').modal('hide');
+    $('#input-text').addClass('hide');
+    $('#output-resp').removeClass('hide');
+    
+    for (var t = 0; t < currentTask.translations.length; t++){
+	var text = "";
+	var trans = currentTask.translations[t];
+	var elem = $('<span>');
+	if (separators_str.indexOf(trans.word) < 0) {
+	    elem.addClass('word-label');	
+	    if (trans.pron != null && trans.pron.length > 0) {
+		for (var p = 0; p < trans.pron.length; p++) {
+		    var subelem = $('<span>');
+		    var tmp = trans.pron[p].pron;
+		    if(tmp == "") tmp = trans.word;
+		    subelem.html(tmp);
+		    subelem.addClass('translated');
+		    subelem.attr('data-id-word', t);
+		    subelem.attr('data-id-pron', p);
+		    elem.append(subelem);
+		}
+	    }
+	    else {
+		elem.html(trans.word);
+	    }
+	}
+	else {
+	    elem.html(trans.word);
+	}
+
+	$('#div-res').append(elem);
+    }
+
+    $('.translated').click(loadDefinition);
+}
+
+
+function succHandler(data) {
+    var res = processRes(data);
+    currentTask.push_result(res);
+    updateProgressbar();
+    
+    runTaskStep();
+
+    
+}
+
+function updateProgressbar() {
+    var progr = currentTask.get_progress();
+    $('#progress').css('width', progr + "%");
+}
+
+
+function getEmptyItem(word) {
+    var item = {
+	word: word,
+	audio: null,
+	pron: null
+    };
+    return item;
+}
+
+function processRes(data) {
+    data = data.replace(/<img[^>]*>/g,"");
+    htmlObj = $(data);
+    var res = null;
+    var audio = null;
+   
+    var elems = htmlObj.find(ENTRY_CLASS);
+    if (elems.length > 0) res = [];
+    for (var e = 0; e < elems.length; e++) {	
+	var item = processEntry(elems.eq(e))
+	res.push(item);
+    }
+    var audio_elems = htmlObj.find('audio');
+    if (audio_elems.length > 0) {
+	audio = audio_elems[0];
+    }
+
+    audio_item = audio;
+    var item = {
+	word : currentTask.get_current_word(),
+	audio : audio,
+	pron : res
+    };
+    
+    return item;
+}
+
+function loadDefinition() {
+    var cont = $('#definitions');
+    cont.empty();
+    var elem = $(this);
+    var idw = elem.attr('data-id-word');
+    var idp = elem.attr('data-id-pron');
+
+    var word = currentTask.translations[idw].word;
+    var audio = currentTask.translations[idw].audio;
+    var item = currentTask.translations[idw].pron[idp];
+    var pron = item.pron;
+    for (var d = 0; d < item.definitions.length; d++) {
+	var p = $('<p>');
+	p.html(item.definitions[d]);
+	cont.append(p);
+	cont.append($('<hr>'));
+    }
+    
+    $('#word-title').html(word + " " + pron + " ");
+    $('#word-title').attr('data-id-word', idw);
+    if(audio != null) {
+	var icon = $('<i>');
+	icon.addClass('fa');
+	icon.addClass('fa-volume-up');
+	$('#word-title').append(icon);
+    }
+    $('#def-modal').modal();
+}
+
+function handlerAudio() {
+    var elem = $(this);
+    var idw = elem.attr('data-id-word');
+    if (idw != null || idw != "") {
+	var audio = currentTask.translations[idw].audio;
+	if (audio != null)
+	    playAudio(audio);
+    }
+}
+
+
+function playAudio(audio_item) {
+    var audio = null;
+
+    var src = audio_item.currentSrc;
+    src = src.substr(src.indexOf('/audio'));
+    src = 'http://' + BASE_URL + src;
+ 
+    audio = new Audio(src);
+    audio.play(audio);
+    return audio;
+}
+
+function processEntry(entry) {
+    var pron = null;
+    var definitions = [];
+    var pron_elems = entry.find(PRON_CLASS);
+    var tmp_arr = [];
+    for (var p = 0; p < pron_elems.length; p++) {
+	tmp_arr.push(pron_elems.eq(p).text());
+    }
+    pron = tmp_arr.join(' | ');
+    
+    var def = entry.find(SINGLE_DEF_CLASS).text();
+    if (def != "") {
+	definitions.push(def);
+    }
+    else {
+	var def_items = entry.find(MULTI_DEF_CLASS);
+	for (var d = 0; d < def_items.length; d++) {
+	    var def_text = def_items.eq(d).text();
+	    definitions.push(def_text);
+	}	
+    }
+
+    var res = {
+	pron : pron,
+	definitions : definitions
+    };
+
+    return res;
+}
+
+function errHandler(data) {
+    console.log('ERROR');
+    console.log(data);
+}
+
+
+function resetTask () {    
+    currentTask.words = [];
+    currentTask.processed_words = 0;
+    currentTask.translations = [];
+
+    updateProgressbar();
+    $('#div-res').empty();
+    $('#output-resp').addClass('hide');
+    $('#input-text').removeClass('hide');
+}
+
+
+function updateTask(token) {
+    currentTask.words.push(token);	
+}
+
+
+
+$(document).ready(function() {
+    $('#btn-translate').click(translate);
+    $('#btn-back').click(resetTask);
+    $('#word-title').click(handlerAudio);
+});
